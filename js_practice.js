9d11a8bd0ace1431a732f1fac1901d241b7993f2
RobertNjogu <kimanyar@yahoo.com>
2022-11-08 18:27:47 -0500

    Added js simple js file and few heading edits on index.html

---
+++ js_practice.js
@@ -0,0 +1,13 @@
+function greetings(name) {
+  return console.log("Hello " + name);
+}
+
+greetings("Clara");
+greetings("Mary");
+
+function changeToKgs(kg) {
+  kg = kg * 1000;
+  return console.log(kg);
+}
+changeToKgs(20);
+changeToKgs(30);
